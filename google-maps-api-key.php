89d5454161ab8f85f0a93b829b04917bd75e6808
stiofan <info@wpgeodirectory.com>
2016-06-24 22:39:25 +0100

    initial commit

---
+++ google-maps-api-key.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * This is the main GeoDirectory plugin file, here we declare and call the important stuff
+ *
+ * @package     GMAPIKEY
+ * @copyright   2016 AyeCode Ltd
+ * @license     GPL-2.0+
+ * @since       1.0.0
+ *
+ * @wordpress-plugin
+ * Plugin Name: Google Maps API KEY
+ * Plugin URI: http://wpgeodirectory.com/
+ * Description: Adds API KEY to Google maps calls if they have been enqueue correctly.
+ * Version: 1.0.0
+ * Author: Stiofan
+ * Author URI: http://wpgeodirectory.com
+ * Text Domain: google-maps-api-key
+ * Domain Path: /languages
+ * Requires at least: 3.1
+ * Tested up to: 4.5
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * The current version number.
+ *
+ * @since 1.0.0
+ */
+define( "GMAPIKEY_VERSION", "1.0.0" );
+
+
+
+add_action( 'plugins_loaded', 'myplugin_load_textdomain' );
+/**
+ * Load plugin textdomain.
+ *
+ * @since 1.0.0
+ */
+function myplugin_load_textdomain() {
+	load_plugin_textdomain( 'google-maps-api-key', false, plugin_basename( dirname( __FILE__ ) ) . '/languages' );
+}
+
+
+add_filter( 'clean_url', 'rgmk_find_add_key', 99, 3 );
+/**
+ * Clean url.
+ *
+ * @since   1.0.0
+ * @package GMAPIKEY
+ *
+ * @param string $url          Url.
+ * @param string $original_url Original url.
+ * @param string $_context     Context.
+ *
+ * @return string Modified url.
+ */
+function rgmk_find_add_key( $url, $original_url, $_context ) {
+	$key = get_option( 'rgmk_google_map_api_key' );
+
+	// If no key added no point in checking
+	if ( ! $key ) {
+		return $url;
+	}
+
+	if ( strstr( $url, "maps.google.com/maps/api/js" ) !== false ) {// it's a Google maps url
+
+		if ( strstr( $url, "key=" ) === false ) {// it needs a key
+			$url = $url . "&key=" . $key;
+			$url = str_replace( "&#038;", "&amp;", $url ); // or $url = $original_url
+		}
+
+	}
+
+	return $url;
+}
+
+add_action( 'admin_menu', 'rgmk_add_admin_menu' );
+
+/**
+ * Add the admin menu link.
+ *
+ * @since   1.0.0
+ * @package GMAPIKEY
+ */
+function rgmk_add_admin_menu() {
+	add_submenu_page( 'options-general.php', 'Google API KEY', 'Google API KEY', 'manage_options', 'google-maps-api-key', 'rgmk_add_admin_menu_html' );
+}
+
+/**
+ * The html output for the settings page.
+ *
+ * @since   1.0.0
+ * @package GMAPIKEY
+ */
+function rgmk_add_admin_menu_html() {
+	$updated = false;
+	if ( isset( $_POST['rgmk_google_map_api_key'] ) ) {
+		$key     = esc_attr( $_POST['rgmk_google_map_api_key'] );
+		$updated = update_option( 'rgmk_google_map_api_key', $key );
+	}
+
+	if ( $updated ) {
+		echo '<div class="updated fade"><p><strong>' . __( 'Kay Updated!', 'google-maps-api-key' ) . '</strong></p></div>';
+
+	}
+	?>
+	<div class="wrap">
+
+		<h2><?php _e( 'Retro Add Google Maps API KEY', 'google-maps-api-key' ); ?></h2>
+		<p><?php _e( 'This plugin will attempt to add your Google API KEY to any Google Maps JS file that has properly been enqueue.', 'google-maps-api-key' ); ?></p>
+		<p><?php echo sprintf( __( 'To Get a Google Maps API KEY %sclick here%s', 'geodirectory' ), '<a target="_blank" href=\'https://console.developers.google.com/flows/enableapi?apiid=maps_backend,geocoding_backend,directions_backend,distance_matrix_backend,elevation_backend&keyType=CLIENT_SIDE&reusekey=true\'>', '</a>' ) ?></p>
+
+		<form method="post" action="options-general.php?page=google-maps-api-key">
+			<label for="rgmk_google_map_api_key"><?php _e( 'Enter Google Maps API KEY', 'google-maps-api-key' ); ?></label>
+			<input title="<?php _e( 'Add Google Maps API KEY', 'google-maps-api-key' ); ?>" type="text"
+			       name="rgmk_google_map_api_key" id="rgmk_google_map_api_key"
+			       value="<?php echo esc_attr( get_option( 'rgmk_google_map_api_key' ) ); ?>"/>
+			<?php
+
+			submit_button();
+
+			?>
+		</form>
+
+	</div><!-- /.wrap -->
+	<?php
+}
---
+++ readme.txt
@@ -0,0 +1,52 @@
+=== Google Maps API KEY ===
+Contributors: stiofansisland, paoltaia
+Donate link: https://wpgeodirectory.com
+Tags:  Google Maps, Google Maps KEY, Google Maps API KEY
+Requires at least: 3.1
+Tested up to: 4.5
+Stable tag: 1.0.0
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Retroactively add Google Maps API KEY to any theme or plugin.
+
+== Description ==
+
+Retroactively add Google Maps API KEY to any theme or plugin.
+
+== Installation ==
+
+= Minimum Requirements =
+
+* WordPress 3.1 or greater
+* PHP version 5.2.4 or greater
+* MySQL version 5.0 or greater
+
+= Automatic installation =
+
+Automatic installation is the easiest option. To do an automatic install log in to your WordPress dashboard, navigate to the Plugins menu and click Add New.
+
+In the search field type Google Maps API KEY and click Search Plugins. Once you've found the plugin you install it by simply clicking Install Now.
+
+= Manual installation =
+
+The manual installation method involves downloading the plugin and uploading it to your webserver via your favourite FTP application. The WordPress codex will tell you more [here](http://codex.wordpress.org/Managing_Plugins#Manual_Plugin_Installation).
+
+= Updating =
+
+Automatic updates should seamlessly work. We always suggest you backup up your website before performing any automated update to avoid unforeseen problems.
+
+== Frequently Asked Questions ==
+
+Ask and they shall be answered
+
+== Screenshots ==
+
+
+== Changelog ==
+
+= 1.0.0 =
+
+initial release
+
+== Upgrade Notice ==
